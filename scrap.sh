1e96c68b6844cc4a0c36748c01f27d7a463df3e1
zackad <zackad.dev@gmail.com>
2017-10-21 17:49:17 +0700

    put scrapped data inside directory

--- scrap.sh
+++ scrap.sh
@@ -4,7 +4,9 @@
 SCRIPTPATH=$(readlink -f "$0")
 DIRNAME=$(dirname "$SCRIPTPATH")
 
+makedir -p prakiraan-angin
+
 wget http://cdn.bmkg.go.id/datamkg/meteorologi/images/streamline_d1.jpg -O /tmp/wind_result.jpg
 temp_name=$(crc32 /tmp/wind_result.jpg)
-file_name="$DIRNAME/$(date +%Y-%m-%d)_$temp_name.jpg"
+file_name="$DIRNAME/prakiraan-angin/$(date +%Y-%m-%d)_$temp_name.jpg"
 mv /tmp/wind_result.jpg $file_name
